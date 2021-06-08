3775f19addb95450e3966b6216fc3aa9b861d580
soltanoff <piccadillable@gmail.com>
2021-06-08 23:20:57 +0300

    added sqlalchemy integration (use sqlite)

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+import databases
+import sqlalchemy
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 import settings
-from routers import index
+import tables
+from routers import index, articles
 
 app = FastAPI()
 app.include_router(index.router)
+app.include_router(articles.router)
 app.mount(settings.STATIC_URL, StaticFiles(directory=settings.STATIC_DIRECTORY), name="static")
+
+database = databases.Database(settings.DATABASE_URL)
+engine = sqlalchemy.create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
+tables.metadata.create_all(engine)
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
--- settings.py
+++ settings.py
@@ -1,4 +1,7 @@
 STATIC_URL = '/static/'
 STATIC_DIRECTORY = 'static'
+
 TEMPLATE_DIRECTORY = 'templates'
 BASE_TEMPLATE_NAME = 'index.html'
+
+DATABASE_URL = "sqlite:///./db.sqlite3"
