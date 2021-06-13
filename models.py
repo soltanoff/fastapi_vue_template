9272bf55682fde2ea4afac34441878f42bdfd36a
soltanoff <piccadillable@gmail.com>
2021-06-13 16:58:28 +0300

    added pagination model

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from pydantic import BaseModel
 
 
@@ -10,3 +12,13 @@ class Article(BaseModel):
 class ArticleIn(BaseModel):
     title: str
     content: str
+
+
+class PaginationInfo(BaseModel):
+    number: str
+    link: str
+
+
+class PaginatedArticles(BaseModel):
+    pages_info = List[PaginationInfo]
+    articles = List[Article]
