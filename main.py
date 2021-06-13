a67e06b6fa83bd23b0f2121def38ae6b3e6d9b1b
soltanoff <piccadillable@gmail.com>
2021-06-13 17:48:51 +0300

    added trusted host middleware

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from starlette.middleware.trustedhost import TrustedHostMiddleware
 
 import settings
 from routers import index, articles
 from tables import database
 
 app = FastAPI()
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=settings.ALLOWED_HOSTS)
 app.include_router(index.router)
 app.include_router(articles.router)
 app.mount(settings.STATIC_URL, StaticFiles(directory=settings.STATIC_DIRECTORY), name="static")
