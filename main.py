7e8e0fa4a0a5d01661f8403e1d05e44521e8fcb2
soltanoff <piccadillable@gmail.com>
2021-06-13 15:20:19 +0300

    rearrange tables code

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-import databases
-import sqlalchemy
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 import settings
-import tables
 from routers import index, articles
+from tables import database
 
 app = FastAPI()
 app.include_router(index.router)
 app.include_router(articles.router)
 app.mount(settings.STATIC_URL, StaticFiles(directory=settings.STATIC_DIRECTORY), name="static")
-
-database = databases.Database(settings.DATABASE_URL)
-engine = sqlalchemy.create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
-tables.metadata.create_all(engine)
 
 
 @app.on_event("startup")
