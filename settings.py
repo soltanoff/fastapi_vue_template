2c6a8655fba541a1152c29e1572bd0626177e04a
soltanoff <piccadillable@gmail.com>
2021-06-14 17:42:37 +0300

    upd settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,14 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 PAGE_SIZE = 5
 
 STATIC_URL = '/static/'
-STATIC_DIRECTORY = 'static'
+STATIC_DIRECTORY = os.path.join(BASE_DIR, 'static')
 
-TEMPLATE_DIRECTORY = 'templates'
+TEMPLATE_DIRECTORY = os.path.join(BASE_DIR, 'templates')
 BASE_TEMPLATE_NAME = 'index.html'
 
 DATABASE_URL = "sqlite:///./db.sqlite3"
